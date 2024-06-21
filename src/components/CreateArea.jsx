23b1188bc8ddd862a3164349211a046fbc83c6b2
Doean Maure <gilynmaure@gmail.com>
2024-06-21 21:53:16 +0800

    complete creating new note

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,13 +1,40 @@
 import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
 
 function CreateArea() {
+    
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event){
+        const { name, value } = event.target;
+
+        setNote(prevNote => {
+            return {
+                ...prevNote, 
+                [name]: value
+            };
+        });
+    }
     
     return (
         <>
         <div>
             <form className="create-note">
-                <input name="title" placeholder="Title"/>
-                <textarea name="content" placeholder="Take a note..."/>
+                <input 
+                name="title"
+                onChange={handleChange}
+                value={note.title} 
+                placeholder="Title"
+                />
+                <textarea 
+                name="content" 
+                onChange={handleChange}
+                value={note.content}
+                placeholder="Take a note..."
+                />
                 <button><AddIcon/></button>
             </form>
         </div>
